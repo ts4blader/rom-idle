ba4ca05a1c90d6e5099626d564ed8259f90d911a
ts4blader <ts4blader@gmail.com>
2021-08-04 15:44:56 +0700

    add active class

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <div className="nav">
       <ul>
-        <li className="nav__link">Home</li>
+        <li className="nav__link active">Home</li>
         <li className="nav__link">About</li>
         <li className="nav__link">Contact</li>
       </ul>
