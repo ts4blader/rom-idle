2094ecf0aa3d20d985f1bce314755a1413ac3189
ts4blader <ts4blader@gmail.com>
2021-08-04 15:44:10 +0700

    add header to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header";
+import "./scss/main.scss";
+
 function App() {
   return (
     <div className="app">
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <Header />
+      </div>
     </div>
   );
 }
