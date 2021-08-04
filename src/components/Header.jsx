e8bfe8e1e19365dfad64bb78dcff832e72fc0f15
ts4blader <ts4blader@gmail.com>
2021-08-04 15:24:23 +0700

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Icon from "../components/Icon";
+import Nav from "../components/Nav";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="logo">
+          <Icon src="logo.png" alt="LOGO" />
+          <Icon src="logo.png" alt="LOGO" />
+        </div>
+        <Nav />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <ul>
+        <li className="nav__link">Home</li>
+        <li className="nav__link">About</li>
+        <li className="nav__link">Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
