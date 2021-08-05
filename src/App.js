6d3bb97ab4621e39004f3a1ef5a22cca1ed2b4b3
ts4blader <ts4blader@gmail.com>
2021-08-05 11:01:16 +0700

    generate other sections

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import Header from "./components/Header";
 import Social from "./components/Social";
 import Section from "./components/Section";
 import PSP from "./data/psp";
+import SNES from "./data/snes";
+import N3DS from "./data/n3ds";
 import "./scss/main.scss";
 
 function App() {
+  const content = [
+    {
+      gear: "psp",
+      data: PSP,
+    },
+    {
+      gear: "snes",
+      data: SNES,
+    },
+    {
+      gear: "n3ds",
+      data: N3DS,
+    },
+  ];
+
   return (
     <div className="app" id="app">
       <div className="wrapper">
         <Header />
         <Social />
-        <Section content={PSP} gear="psp" />
+        {content.map((item, index) => (
+          <Section content={item.data} gear={item.gear} key={index} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/Indicator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Indicator({ length }) {
+  return <div className="indicator"></div>;
+}
+
+export default Indicator;
