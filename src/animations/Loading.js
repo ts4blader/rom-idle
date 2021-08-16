2f486f8a2a6934a00409b9edbe408ddd83fdde42
ts4blader <ts4blader@gmail.com>
2021-08-16 13:44:31 +0700

    style and config loading

---
+++ src/animations/Loading.js
@@ -0,0 +1,62 @@
+import { gsap } from "gsap";
+
+var tl = gsap.timeline({ repeat: -1, yoyo: false, repeatDelay: 0.25 });
+const LoadingAnimation = () => {
+  tl.staggerTo(
+    ".text .dot",
+    0.5,
+    {
+      y: -50,
+      ease: "back",
+    },
+    0.25
+  );
+  tl.staggerTo(
+    ".text .dot",
+    0.5,
+    {
+      y: 0,
+      ease: "Bounce.easeOut",
+    },
+    0.25
+  );
+};
+
+export const CompleteLoading = () => {
+  let complete = gsap.timeline({});
+  complete
+    .to(".loading .logo, .loading__text", 1, {
+      yPercent: "-100",
+      ease: "Expo.easeOut",
+    })
+    .to(
+      ".loaded",
+      1,
+      {
+        opacity: 1,
+        top: "110%",
+        ease: "Expo.easeOut",
+      },
+      "0.5"
+    )
+    .to(".loading .text, .loaded", 1, {
+      opacity: 0,
+      y: -300,
+      ease: "Expo.easeOut",
+    })
+    .to(
+      ".loading",
+      1,
+      {
+        height: 0,
+        ease: "Expo.easeOut",
+      },
+      "2"
+    );
+};
+
+export const LoadingPause = () => {
+  tl.pause();
+};
+
+export default LoadingAnimation;
