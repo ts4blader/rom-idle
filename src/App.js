064e51878a193d0809a2fbb77362c94964c15f16
ts4blader <ts4blader@gmail.com>
2021-08-22 14:49:11 +0700

    move main to function comps

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
-import Section from "./components/Section";
-import Indicator from "./components/Indicator";
-import FeedBack from "./components/FeedBack";
 import Overlay from "./components/Overlay";
 import Loading from "./components/Loading";
 import Cursor from "./components/Cursor";
-import PSP from "./data/psp";
-import SNES from "./data/snes";
-import N3DS from "./data/n3ds";
+import Main from "./components/Main";
 import "./scss/main.scss";
 
 function App() {
-  const main = React.useRef(null);
-  const content = [PSP, SNES, N3DS];
-
-  const scroll = (n) => {
-    main.current.scrollTo({
-      top: n * main.current.clientHeight,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
-
   return (
     <div className="app" id="app">
       <Cursor />
@@ -30,13 +14,7 @@ function App() {
       <Overlay />
       <div className="wrapper">
         <Header />
-        <main ref={main}>
-          {content.map((item, index) => (
-            <Section content={item} key={index} />
-          ))}
-          <FeedBack />
-          <Indicator length={content.length} scrollTo={scroll} />
-        </main>
+        <Main />
       </div>
     </div>
   );
---
+++ src/components/Main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Section from "../components/Section";
+import Indicator from "../components/Indicator";
+import FeedBack from "../components/FeedBack";
+import PSP from "../data/psp";
+import SNES from "../data/snes";
+import N3DS from "../data/n3ds";
+
+function Main() {
+  const main = React.useRef(null);
+  const content = [PSP, SNES, N3DS];
+
+  const scroll = (n) => {
+    main.current.scrollTo({
+      top: n * main.current.clientHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <main ref={main}>
+      {content.map((item, index) => (
+        <Section content={item} key={index} />
+      ))}
+      <FeedBack />
+      <Indicator length={content.length} scrollTo={scroll} />
+    </main>
+  );
+}
+
+export default Main;
