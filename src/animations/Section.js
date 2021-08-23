1dafbaefec2a19689b929d61de9bc5dd1e7224c8
ts4blader <ts4blader@gmail.com>
2021-08-23 20:49:46 +0700

    complete animation for each section

--- src/animations/Section.js
+++ src/animations/Section.js
@@ -80,3 +80,83 @@ export const IndicatorActive = (gear) => {
     },
   });
 };
+
+// Animation with each section
+const scrollTrigger = (trigger) => {
+  return {
+    trigger: trigger,
+    scroller: ".main",
+    start: "top 10%",
+    end: "center 40%",
+    toggleActions: "play reverse play reverse",
+  };
+};
+
+export const moveIn = (trigger) => {
+  //container
+
+  gsap.from(`${trigger} .section__inner`, {
+    duration: 2.5,
+    opacity: 0,
+    y: 500,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+  });
+  //Content
+  gsap.from(`${trigger} .section__title`, {
+    duration: 1,
+    opacity: 0,
+    y: 500,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+  });
+  gsap.from(`${trigger} .section__subtitle`, {
+    duration: 1.5,
+    opacity: 0,
+    y: 300,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+    delay: 0.5,
+  });
+  gsap.from(`${trigger} .section__description`, {
+    duration: 1,
+    opacity: 0,
+    y: 500,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+    delay: 0.2,
+  });
+  gsap.from(`${trigger} .button`, {
+    duration: 1,
+    opacity: 0,
+    y: 1000,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+    delay: 0.3,
+  });
+
+  //gallery animation
+  gsap.from(`${trigger} .section__gallery__item`, {
+    duration: 0.8,
+    opacity: 0,
+    y: 1500,
+    ease: "Expo.easeOut",
+    scrollTrigger: scrollTrigger(trigger),
+    stagger: 0.3,
+  });
+  gsap.from(`${trigger} .section__gallery`, {
+    duration: 0.5,
+    opacity: 0,
+    scrollTrigger: scrollTrigger(trigger),
+    delay: 0.3,
+  });
+  //rom name animation
+  gsap.from(`${trigger} .section__rom-name .bar`, {
+    duration: 1,
+    y: 300,
+    opacity: 0,
+    scrollTrigger: scrollTrigger(trigger),
+    delay: 0.2,
+    stagger: 0.2,
+  });
+};
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Button from "../components/Button";
 import Image from "../components/Image";
 import { StoreContext } from "../Store";
-import { showBG, hideBG } from "../animations/Section";
+import { showBG, hideBG, moveIn } from "../animations/Section";
 
 function Section({ content }) {
   const [state, dispatch] = React.useContext(StoreContext);
   const [current, setCurrent] = React.useState(0);
 
   React.useEffect(() => {
-    //TODO: init all animation
+    //TODO: init animation re-render with current slide
     let show = showBG(content.gear, content.widths);
     let hide = hideBG(content.gear, () => {
       //* On Complete Function
@@ -25,6 +25,11 @@ function Section({ content }) {
     };
   }, [current]);
 
+  React.useEffect(() => {
+    //TODO init animation one time
+    moveIn(`.${content.gear}-section`);
+  }, []);
+
   const image = require("../res/images/" + content.images[current]).default;
 
   const bgStyle = {
@@ -35,50 +40,48 @@ function Section({ content }) {
 
   return (
     <section className={`${content.gear}-section section`}>
+      {/* Background section  */}
       <div className="section__bg" style={bgStyle}>
         <div className="section__bg__overlay"></div>
       </div>
-      <div
-        className="container"
-        style={{
-          marginTop: state.headerHeight + "px",
-        }}
-      >
-        {/* Background section  */}
-
-        <div className="row section__caption">
-          <p className="section__subtitle">{content.subtitle}</p>
-          <h3 className="section__title">{content.title}</h3>
-        </div>
-
-        <p className="section__description">{content.description}</p>
-
-        <Button text={content.cta} />
-
-        <div className="row bottom">
-          {/* gallery  */}
-          <div className="section__gallery row hide-on-md">
-            {content.images.map((item, index) => {
-              const className =
-                current === index
-                  ? "col section__gallery__item active"
-                  : "col section__gallery__item";
-              return (
-                <div
-                  className={className}
-                  key={index}
-                  onClick={() => setCurrent(index)}
-                >
-                  <Image src={item} />
-                </div>
-              );
-            })}
+      <div className="section__inner">
+        <div
+          className="container"
+          style={{
+            marginTop: state.headerHeight + "px",
+          }}
+        >
+          <div className="row section__caption">
+            <p className="section__subtitle">{content.subtitle}</p>
+            <h3 className="section__title">{content.title}</h3>
           </div>
-          {/* ROM name  */}
-          <div className="section__rom-name">
-            <div className="bar hide-on-sm"></div>
-            <p className="text">{content.roms[current]}</p>
-            <div className="bar hide-on-sm"></div>
+          <p className="section__description">{content.description}</p>
+          <Button text={content.cta} />
+          <div className="row bottom">
+            {/* gallery  */}
+            <div className="section__gallery row hide-on-md">
+              {content.images.map((item, index) => {
+                const className =
+                  current === index
+                    ? "col section__gallery__item active"
+                    : "col section__gallery__item";
+                return (
+                  <div
+                    className={className}
+                    key={index}
+                    onClick={() => setCurrent(index)}
+                  >
+                    <Image src={item} />
+                  </div>
+                );
+              })}
+            </div>
+            {/* ROM name  */}
+            <div className="section__rom-name">
+              <div className="bar hide-on-sm"></div>
+              <p className="text">{content.roms[current]}</p>
+              <div className="bar hide-on-sm"></div>
+            </div>
           </div>
         </div>
       </div>
