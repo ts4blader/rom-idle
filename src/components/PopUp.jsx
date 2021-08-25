6dd9ad3634166a243aa558842c930f9dbf583442
ts4blader <ts4blader@gmail.com>
2021-08-25 21:28:12 +0700

    fixed overlay on clese

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { StoreContext } from "../Store";
-import { popUp } from "../Action";
+import ACTION from "../Action";
 
 function PopUp() {
   const [state, dispatch] = React.useContext(StoreContext);
+
+  const close = React.useCallback(() => {
+    dispatch({ type: ACTION.popUp.hide });
+    dispatch({ type: ACTION.overlay.close });
+  }, []);
 
   return (
     <div className="pop-up" state={state.showPopUp ? "show" : "hide"}>
       {/* <p className="pop-up__title">Title</p> */}
       <p className="pop-up__message">Thank for visited!</p>
-      <div className="ok-btn" onClick={() => dispatch({ type: popUp.hide })}>
+      <div className="ok-btn" onClick={close}>
         OK
       </div>
     </div>
